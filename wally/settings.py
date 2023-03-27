ac7c61659db9124724a35b84990791821c7ed35f
kn4y4n <kn4y4nnn@gmail.com>
2023-03-27 21:23:22 +0530

    feat: auth model

--- wally/settings.py
+++ wally/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = "authentication.User"
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party libraries
+    "rest_framework",
+    # local apps
+    "authentication",
 ]
 
 MIDDLEWARE = [
