0d0acdd150bc22fce43ed9f65f2e995dd1538952
kn4y4n <kn4y4nnn@gmail.com>
2023-04-01 02:12:03 +0530

    fix: jwt token path

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from .views import (
     RegisterView,
     VerifyEmail,
@@ -11,6 +15,8 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("verify-email/", VerifyEmail.as_view(), name="verify-email"),
     path("login/", LoginView.as_view(), name="login"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path(
         "password-reset-request/",
         PasswordResetRequest.as_view(),
